e5ea601a63e43f421f56baf167641b42fb846b5f
salonikalsekar <saloniskalsekar@gmail.com>
2019-03-07 20:32:10 -0700

    mvc_2

--- models/product.js
+++ models/product.js
@@ -1,15 +1,40 @@
-const products = [];
 path = require('path')
+        let products = [];
+const p = path.join(path.dirname(process.mainModule.filename), 'data', 'products.json' );
+
+const fs = require('fs')
 module.exports = class Product{
     constructor(t){
         this.title = t;
 
     }
+    
     save(){
-        const p = path.join(path.dirname(process.mainModule.filename));
-    }
+        fs.readFile(p, (err, fileContent) =>{
+            console.log(fileContent)
 
-    static fetchAll(){
-        return products;
+            if(!err){
+                products = JSON.parse(fileContent)
+            }
+            products.push(this)
+                fs.writeFile(p, JSON.stringify(products), (err)=>{
+                    if(err)
+                    console.log(err)
+                });
+            
+        });
+    };
+
+    static fetchAll(cb){
+        fs.readFile(p, (err, fileContent)=>{
+            if(err){
+            console.log("error reading file")
+            cb([])
+            }
+            else{
+                
+            cb(JSON.parse(fileContent))
+            }
+        })
     }
 }
